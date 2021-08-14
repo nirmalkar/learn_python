f4ca2da570fb2a688fd27a3902bb2aa6e382f19a
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-14 23:48:37 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,14 @@
+print(len("Hello world"))
+
+# subscripting
+print("Hello world"[0])
+
+print(1)
+
+print(type(1) == "int")
+
+# type conversion
+intro = "I am"
+age = 27
+ageInStr = str(age)
+print(intro + ageInStr)
---
+++ sumOfDigits.py
@@ -0,0 +1,6 @@
+twoDigitNum = input("Enter two digit num ")
+strNum = str(twoDigitNum)
+sum = str(int(strNum[0]) + int(strNum[1]))
+
+print("The sum of digits of given number is " + sum)
+
