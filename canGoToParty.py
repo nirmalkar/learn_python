0833125aea16bcac58cb23a23ef8cefa1a1d5ea6
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-15 13:13:18 +0530

    if elif else

---
+++ canGoToParty.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age "))
+
+if age < 16:
+    print("You can not come to the party, Get lost!!")
+elif age > 16 & age < 18:
+    print("You can come to party but can not drink!!")
+else:
+    print("Come enjoy the party!! do whatever you want!!")
+
+
---
+++ oddOrEven.py
@@ -0,0 +1,9 @@
+number = int(input("Please enter an integer "))
+isEven = (number % 2) == 0
+
+if isEven :
+    print("Number is even")
+else:
+    print("Entered Number is odd")
+
+
