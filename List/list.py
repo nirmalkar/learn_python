125f48a0b7f16aef3228823701e69a021b2f7657
nirmalkar <hemantnirmalkar17@gmail.com>
2021-08-21 15:01:32 +0530

    List

---
+++ List/list.py
@@ -0,0 +1,9 @@
+nameList = ["Aman", "Ravi","Hemant", "Linda", "Cassidy", "Richa", "Swati"]
+
+print(nameList[0])
+print(nameList[-1])
+nameList[-1] = "Aditi"
+nameList.append("Swati")
+nameList.extend(["Subham", "Ram", "Kelly"])
+nameList.pop(6)
+print(nameList)
